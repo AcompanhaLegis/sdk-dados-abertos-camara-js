77839ad59eee054ad28ddcb690e0b3f314a2c312
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2020-09-23 22:49:55 +0200

    :bulb: Add get method on api

--- modules/api.js
+++ modules/api.js
@@ -1,5 +1,31 @@
 import axios from 'axios';
+import qs from 'qs';
 
-export default axios.create({
+import { validateOptions, validateFormat } from './utils';
+
+const api = axios.create({
   baseURL: 'https://dadosabertos.camara.leg.br/api/v2',
 });
+
+export const get = async (endpoint, options, availableOrderFields = []) => {
+  if (validateOptions(availableOrderFields, options)) {
+    try {
+      const { format } = options;
+      delete options.format;
+
+      const res = await api.get(`/${endpoint}?${qs.stringify(options)}`, {
+        headers: {
+          Accept: `application/${format}`,
+        },
+      });
+
+      return res;
+    } catch (err) {
+      throw err;
+    }
+  } else {
+    throw new Error('Invalid options passed!');
+  }
+};
+
+export default api;
