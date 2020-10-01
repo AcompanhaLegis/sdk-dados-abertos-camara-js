25935c3f94673765ae0834dfb87590f90d29c734
Raúl <raulguzmanguerrero@outlook.com>
2020-10-01 18:08:27 -0500

    Added partidos support to sample.js

--- sample.js
+++ sample.js
@@ -15,6 +15,10 @@ const main = async () => {
   const eventosDeputado = await CamaraAPI.getDeputadoEventos(deputadoId);
   const frentes = await CamaraAPI.getDeputadoFrentes(deputadoId);
   const orgaosDeputado = await CamaraAPI.getDeputadoOrgaos(deputadoId);
+  const partidos = await CamaraAPI.getPartidos();
+  const partidoId = partidos.dados[0].id;
+  const partido = await CamaraAPI.getPartido(partidoId);
+  const membrosPartido = await CamaraAPI.getMembrosbyPartido(partidoId);
 
   console.log('deputados', deputados);
   console.log('deputado', deputado);
@@ -22,6 +26,10 @@ const main = async () => {
   console.log('discursos', discursos);
   console.log('eventos deputado', eventosDeputado);
   console.log('frentes', frentes);
+  console.log('partidos', partidos);
+  console.log('partido', partido);
+  console.log('membros partido', membrosPartido);
+
 };
 
 main();
