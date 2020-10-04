d07a96b1298b4907ed5991fca49addb2f85c86cd
Leonardo Camargo <46382008+LeonardoCamargo31@users.noreply.github.com>
2020-10-04 09:10:25 -0300

    :bulb: Add /frentes wrapper
    * create method getFrentes
    
    * create method getFrentesByParlamentar
    
    * create method getFrenteMembros
    
    * fix after lint
    
    Co-authored-by: Jose Eduardo Kimura <j.eduardokimura@icloud.com>

---
+++ modules/frentes.js
@@ -0,0 +1,92 @@
+import { get } from './api';
+
+const availableOrderFields = ['idLegislatura'];
+const availableOptions = ['idLegislatura'];
+
+const defaultPagination = {
+  pagina: 1,
+};
+
+/**
+ * Wraps frentes endpoint.
+ *
+ * @param {Object} options - Options to be sent in the request.
+ * @param {String} format - Desired response format, default is json.
+ * @param {Boolean} fullResponse - If true it will retrieve the whole response object, otherwise it will return only the data object inside the response.
+ */
+export const getFrentes = async (
+  options = defaultPagination,
+  format = 'json',
+  fullResponse = false
+) => {
+  try {
+    const res = await get(
+      'frentes',
+      { ...options, format },
+      availableOrderFields,
+      availableOptions
+    );
+
+    return fullResponse ? res : res.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+/**
+ * Wraps frentes getFrentesByParlamentar.
+ *
+ * @param {Integer} id - ID of the parlamentar that will be requested.
+ * @param {String} format - Desired response format, default is json.
+ * @param {Boolean} fullResponse - If true it will retrieve the whole response object, otherwise it will return only the data object inside the response.
+ */
+export const getFrentesByParlamentar = async (
+  id,
+  format = 'json',
+  fullResponse = false
+) => {
+  try {
+    const res = await get(
+      `frentes/${id}`,
+      { format },
+      availableOrderFields,
+      availableOptions
+    );
+
+    return fullResponse ? res : res.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+/**
+ * Wraps frentes getFrentesByParlamentar.
+ *
+ * @param {Integer} id - ID of the parlamentar that will be requested.
+ * @param {String} format - Desired response format, default is json.
+ * @param {Boolean} fullResponse - If true it will retrieve the whole response object, otherwise it will return only the data object inside the response.
+ */
+export const getFrenteMembros = async (
+  id,
+  format = 'json',
+  fullResponse = false
+) => {
+  try {
+    const res = await get(
+      `frentes/${id}/membros`,
+      { format },
+      availableOrderFields,
+      availableOptions
+    );
+
+    return fullResponse ? res : res.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export default {
+  getFrentes,
+  getFrentesByParlamentar,
+  getFrenteMembros,
+};
