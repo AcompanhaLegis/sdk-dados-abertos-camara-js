d07db32cf0331af7ff4c42a33268d7bda5132766
clockwerkz <clockwerkz@gmail.com>
2020-10-04 23:01:44 -0700

    providing tests for validateOptions and validateFormat

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,8 +1,63 @@
-import { validateFormat } from '../modules/utils';
+import { validateFormat, validateOptions } from '../modules/utils';
 
-describe('tests validateFormat function:', ()=> {
-    test('calling validateFormat with no argument should return false', ()=>{
+describe('validateFormat:', ()=> {
+    test('providing no argument should return true', ()=>{
         let res = validateFormat();
+        expect(res).toBeTruthy();
+    });
+    test('"json" should return true', ()=>{
+        let res = validateFormat('json');
+        expect(res).toBeTruthy();
+    });
+    test('"xml" should return true', ()=>{
+        let res = validateFormat('xml');
+        expect(res).toBeTruthy();
+    });
+    test('passing any other value should return false', ()=>{
+        let res = validateFormat('abcde');
         expect(res).toBeFalsy();
     });
+});
+
+describe("validateOptions testing arguments:", ()=> {
+    test("passing an argument for options should return false",()=>{
+        const res = validateOptions(["fields"], {option: "test"});
+        expect(res).toBeFalsy();
+    });
+    test("not passing an argument for options should return true",()=>{
+        const res = validateOptions(["fields"]);
+        expect(res).toBeTruthy();
+    });
+});
+describe("validateOptions with different options configuration:", ()=>{
+    test("passing an Options object with a key that is the defaultAttributes array should return true", ()=>{
+        const res = validateOptions([], {pagina : true});
+        expect(res).toBeTruthy();
+    });
+    test("passing an Options object with a key that is NOT the defaultAttributes array should return false", ()=>{
+        const res = validateOptions([], {error : true});
+        expect(res).toBeFalsy();
+    });
+    test("passing an Options object with 2 keys, one of which is NOT in attributes should return false",()=>{
+        const res = validateOptions([], {error : true, pagina : true, format: 'json'});
+        expect(res).toBeFalsy();
+    })
+    test("passing an invalid format in the options object should return false",()=>{
+        const res = validateOptions(['name'], {pagina : true, format: 'asd', ordenarPor:'name', test:true}, ["test"]);
+        expect(res).toBeFalsy();
+    })
+});
+describe("validateOptions testing availableOrderFields and moreAttributes:", ()=> {
+    test("passing valid availableOrderFields and options should return true",()=>{
+        const res = validateOptions(['name'], {pagina : true, format: 'json', ordenarPor:'name'});
+        expect(res).toBeTruthy();
+    })
+    test("passing an ordenarPor property with a custom option with no matching attribute should return false",()=>{
+        const res = validateOptions([], {pagina : true, format: 'json', ordenarPor:'name'});
+        expect(res).toBeFalsy();
+    })
+    test("including moreAttributes should return true",()=>{
+        const res = validateOptions(['name'], {pagina : true, format: 'json', ordenarPor:'name', test:true}, ["test"]);
+        expect(res).toBeTruthy();
+    })
 });
